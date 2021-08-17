802728cbe923a0d831ffc772baaa2de814b06444
sachinda <sachinda9696@gmail.com>
2021-08-18 01:15:03 +0530

    Initilize Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"",
+    component:LoginComponent
+
+  },
+  {
+    path:"nav",
+    component:NavBarComponent,
+    children:[
+      {
+        path:"",
+        redirectTo:"dashnoard",
+        pathMatch: "full"
+      },
+
+      {
+        path:"dashnoard",
+        component:DashboardComponent
+      }
+    ]
+  },
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
