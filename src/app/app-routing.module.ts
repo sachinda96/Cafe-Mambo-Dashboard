ae0b9bc43f906a5b5634e6371103eaf5b756b01e
sachinda <sachinda9696@gmail.com>
2021-08-19 01:02:50 +0530

    view order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {OnlineOrdersComponent} from "./online-orders/online-orders.component";
+import {ViewOrderComponent} from "./view-order/view-order.component";
 
 const routes: Routes = [
 
@@ -23,11 +25,35 @@ const routes: Routes = [
 
       {
         path:"dashnoard",
-        component:DashboardComponent
-      }
+        component:DashboardComponent,
+        children: [
+          {
+            path:"onlineorder",
+            component:OnlineOrdersComponent,
+            children:[
+              {
+                path:"vieworder",
+                component:ViewOrderComponent
+              },
+            ]
+          }
+        ]
+      },
+      {
+        path:"onlineorder",
+        component:OnlineOrdersComponent
+      },
+      {
+        path:"vieworder/:id",
+        component:ViewOrderComponent
+      },
     ]
+
   },
-  
+  {
+    path:"onlineorder",
+    component:OnlineOrdersComponent
+  }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { OnlineOrdersComponent } from './online-orders/online-orders.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    NavBarComponent
+    NavBarComponent,
+    OnlineOrdersComponent,
+    ViewOrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
