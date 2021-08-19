42b92fdddaf5f0bf5dd8b134b6fefd75a410a984
Yohanathan-Kugavathanan <kugajpx2012@gmail.com>
2021-08-20 05:15:16 +0530

    added service, model

---
+++ src/app/customer/cust-dashboard/cust-dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustDashboardComponent } from './cust-dashboard.component';
+
+describe('CustDashboardComponent', () => {
+  let component: CustDashboardComponent;
+  let fixture: ComponentFixture<CustDashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CustDashboardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CustDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
